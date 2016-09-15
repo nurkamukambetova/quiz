cbe2e3620af23c0aeef52c4f3ea567ba2125751c
unknown <mukambetova_n@12-08.iuca.loc>
2016-09-15 17:27:29 +0600

    h

---
+++ guiz.py
@@ -0,0 +1,61 @@
+import random 
+n1 = int (raw_input ( "Vveite chislo ot 1 do 6____" ))
+chislo = randit (1,6)
+if n1 > 6:
+    print "vvedite chislo ot 1 do 6"
+elif n1 < 0:
+    print "vedite chislo ot 1 do 6"
+elif n1 == chislo:
+	togda = n1 - chislo
+	print " Vashe kol-vo ballov %d " % bal 
+elif chislo > n1:
+	togda = chislo - n1
+	abs (togda)
+	if togda == 1:
+	    bal = bal + 5
+	    print "U vas ballov %d" % bal
+elif togda == 2:
+	bal = bal + 4
+	print "U vas ballo %d" % bal
+elif togda == 3:
+	bal = bal + 3
+	print "U vas ballo %d" % bal
+elif togda ==4:
+	bal = bal + 2
+	print "U vas ballo %d" % bal
+elif togda == 5:
+	bal = bal + 1
+	print "U vas ballo %d" % bal
+elif chislo < n1:
+	togda = chislo - n1 
+	abs (togda)
+	if togda == 1:
+	        bal = bal + 5
+		print  "U vas ballo %d" % bal
+		print bal
+	elif x == 2:
+		ball = bal + 4
+		print "U vas ballo %d" % bal
+		print bal
+	elif x == 3:
+		ball = bal + 3
+		print "U vas ballo %d" % bal
+		print bal
+	elif x == 4:
+		ball = bal + 2
+		print "U vas ballo %d" % bal
+		print bal
+	elif x == 5:
+		ball = bal + 1
+		print "U vas ballo %d" % bal
+		print bal
+if bal == 25:
+    print "Vy vyigrali"
+else:
+	print "vy proigrali "
+   
+
+
+	
+
+
